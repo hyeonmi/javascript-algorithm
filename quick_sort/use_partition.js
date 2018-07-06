eab82da7d0446d3be0125359474cd4e00c39339f
kim.hyeonmi <kim.hyeonmi@navercorp.com>
2018-07-06 10:04:29 +0900

    feat: write a quicksort with pivot

---
+++ quick_sort/use_partition.js
@@ -0,0 +1,42 @@
+function swap(array, left, right){
+    var leftData = array[left];
+    array[left] = array[right];
+    array[right] = leftData;
+}
+
+function partition(list, left, right, pivot){
+    while(left <= right){
+        while(list[left] < pivot){
+            left++;
+        }
+
+        while(list[right] > pivot){
+            right--;
+        }
+
+        if(left <= right){
+            swap(list, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    return left;
+}
+
+function quickSort(list, left, right){
+    //TODO 정리
+    left = (left === undefined ? 0 : left);
+    right = (right === undefined ? list.length-1 : right);
+
+    if(left >= right){
+        return;
+    }
+
+    var pivot = list[parseInt((left+right)/2, 10)];
+    var index = partition(list, left, right, pivot);
+    quickSort(list, left, index-1);
+    quickSort(list, index, right);
+    return list;
+}
+
